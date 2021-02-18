aae14f2d3c06ac8574ffaa22206793629a4428d2
Sundaram Subramanian <learnwithsundar@gmail.com>
2021-02-18 12:48:02 +0530

    Commiting the Phase I level implementations, Installed the rquired packages and used in required files.

--- ModalPopupWindowWASM/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ ModalPopupWindowWASM/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -82,6 +82,27 @@ using ModalPopupWindowWASM.Shared;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 11 "D:\Projects\ModalPopupWindowBlazorWASM\ModalPopupWindowWASM\_Imports.razor"
+using Blazored;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "D:\Projects\ModalPopupWindowBlazorWASM\ModalPopupWindowWASM\_Imports.razor"
+using Blazored.Modal;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "D:\Projects\ModalPopupWindowBlazorWASM\ModalPopupWindowWASM\_Imports.razor"
+using Blazored.Modal.Services;
+
+#line default
+#line hidden
+#nullable disable
     public partial class App : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
